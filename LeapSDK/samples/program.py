5bf2b258367eaab906658b150af37bae454547a4
Rahul Batra <spyruf@gmail.com>
2018-05-31 20:32:17 -0400

    added research papers"

--- LeapSDK/samples/program.py
+++
@@ -1,5 +0,0 @@
-import os, sys, inspect
-src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
-sys.path.insert(0, lib_dir)
-import Leap
